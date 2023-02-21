b137bc39af62bf57608c0bd12b3645bd4982a9bf
Abdelrahmangithub <abdelrahmanayman000@gmail.com>
2023-02-21 12:33:52 -0800

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints "_putchar"
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+int i;
+char *msg = "_putchar";
+
+for (i = 0; i < 8; i++)
+_putchar(msg[i]);
+
+_putchar(n);
+return (0);
+}
